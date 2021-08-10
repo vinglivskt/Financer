4d5b8740154eab90cb340becda56820b9b31d6c6
Rick-Morty <mr.fremon1200@gmail.com>
2021-08-10 10:08:22 +0300

    Financer

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grey darken-1 empty-layout">
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmptyLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
