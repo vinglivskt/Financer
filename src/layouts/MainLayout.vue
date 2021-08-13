e3d56682c34fa629952f7f5a2065ae022569284a
Rick-Morty <mr.fremon1200@gmail.com>
2021-08-13 20:55:41 +0300

    Financer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="app-main-layout">
+  <div>
+    <Loader v-if="loading"/>
+  <div class="app-main-layout" v-else>
  <Navbar @click="isOpen = !isOpen"/>
     <Sidebar v-model="isOpen"/>
 
@@ -19,18 +21,27 @@
       </router-link>
     </div>
   </div>
+  </div>
 </template>
 
 <script >
 import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
 export default {
-  name:'main-layout',
-  data:()=>({
-    isOpen: true
+  name: 'main-layout',
+  data: () => ({
+    isOpen: true,
+    loading: true
   }),
-  components:{
-Navbar,Sidebar
+  async mounted() {
+    if (!Object.keys(this.$store.getters.info).length) {
+      await this.$store.dispatch('fetchInfo')
+    }
+
+    this.loading = false
+  },
+  components: {
+    Navbar, Sidebar
   }
 }
 
